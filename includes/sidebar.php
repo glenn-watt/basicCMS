892939f5aa2688f4d8fed18ef52f06cef26b2502
Glenn Watt <glennwatt@me.com>
2021-05-18 09:59:45 -0500

    Add search to search.php file and sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,15 +3,17 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <form action="search.php" method="post">
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input type="text" name="search"  class="form-control">
             <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button class="btn btn-default" type="submit" name="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
         </div>
-        <!-- /.input-group -->
+        </form><!--Search form -->
+            <!-- /.input-group -->
     </div>
 
     <!-- Blog Categories Well -->
