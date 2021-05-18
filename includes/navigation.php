b90aecef1f5d8ceede5a4fe7816dd48cb8e5e1f5
Glenn Watt <glennwatt@me.com>
2021-05-18 11:14:30 -0500

    Add link to admin page

--- includes/navigation.php
+++ includes/navigation.php
@@ -24,9 +24,9 @@
                 ?>
 
 
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
 <!--                <li>-->
 <!--                    <a href="#">Services</a>-->
 <!--                </li>-->
