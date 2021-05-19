f0f3286835ee096c6f9d438e3c0dd83e5d7ba9c7
Glenn Watt <glennwatt@me.com>
2021-05-18 20:48:28 -0500

    Refactor queries into functions.php

---
+++ admin/posts.php
@@ -0,0 +1,63 @@
+<?php
+include('includes/admin-header.php');
+?>
+
+
+    <div id="wrapper">
+
+    <!-- Navigation -->
+    <?php
+    include('includes/admin-navigation.php');
+    ?>
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome To Admin
+                        <small>Author</small>
+                    </h1>
+
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>Edwin</td>
+                                <td>Bootstrap Framework</td>
+                                <td>Category</td>
+                                <td>Draft</td>
+                                <td>Image</td>
+                                <td>Tags</td>
+                                <td>Comments</td>
+                                <td>Date</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+<?php
+include ('includes/admin-footer.php');
+?>
