74fc04fc3a606c6388c948f12b6fd64057f09de7
Glenn Watt <glennwatt@me.com>
2021-05-18 20:24:15 -0500

    Refactor queries into functions.php

--- admin/categories.php
+++ admin/categories.php
@@ -54,27 +54,11 @@ include('includes/admin-header.php');
                             <tbody>
                             <?php
                             //SELECT ALL CATEGORIES QUERY
-                            $query = "SELECT * FROM categories";
-                            $select_all_categories_query=mysqli_query($connection,$query);
-                            while($row = mysqli_fetch_assoc($select_all_categories_query)){
-                                $cat_title = $row['cat_title'];
-                                $cat_id = $row['cat_id'];
-                                echo "<tr>";
-                                echo "<td>{$cat_id}</td>";
-                                echo "<td>{$cat_title}</td>";
-                                echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                echo "</tr>";
-                            }
+                            findAllCategories();
                             ?>
                             <?php
                             //DELETE QUERY
-                            if(isset($_GET['delete'])){
-                                $the_cat_id = $_GET['delete'];
-                                $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                                $delete_query = mysqli_query($connection, $query);
-                                header("Location: categories.php");
-                            }
+                            deleteCategries();
                             ?>
                             </tbody>
                         </table>
