8a7a51403b8e3c8ad4ed02db990b2bf51d5967c4
Glenn Watt <glennwatt@me.com>
2021-05-20 15:46:47 -0500

    Add dynamic query display all posts

--- admin/posts.php
+++ admin/posts.php
@@ -36,17 +36,32 @@ include('includes/admin-header.php');
                         </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>10</td>
-                                <td>Edwin</td>
-                                <td>Bootstrap Framework</td>
-                                <td>Category</td>
-                                <td>Draft</td>
-                                <td>Image</td>
-                                <td>Tags</td>
-                                <td>Comments</td>
-                                <td>Date</td>
-                            </tr>
+                            <?php
+                            $query = "SELECT * FROM posts";
+                            $select_all_posts_query=mysqli_query($connection,$query);
+                            while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                                $post_id = $row['post_id'];
+                                $post_author = $row['post_author'];
+                                $post_title = $row['post_title'];
+                                $post_category_id = $row['post_category_id'];
+                                $post_status = $row['post_status'];
+                                $post_image = $row['post_image'];
+                                $post_tags = $row['post_tags'];
+                                $post_comment_count = $row['post_comment_count'];
+                                $post_date = $row['post_date'];
+                            echo "<tr>";
+                                echo "<td>$post_id</td>";
+                                echo "<td>$post_author</td>";
+                                echo "<td>$post_title</td>";
+                                echo "<td>$post_category_id</td>";
+                                echo "<td>$post_status</td>";
+                                echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
+                                echo "<td>$post_tags</td>";
+                                echo "<td>$post_comment_count</td>";
+                                echo "<td>$post_date</td>";
+                            echo "</tr>";
+                            }
+                            ?>
                         </tbody>
                     </table>
                 </div>
