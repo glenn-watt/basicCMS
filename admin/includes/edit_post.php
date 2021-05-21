80b1bdcd9cfd6eab0d6a392ad418d36c615f5901
Glenn Watt <glennwatt@me.com>
2021-05-21 00:54:39 -0500

    Add update query to edit

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -17,6 +17,42 @@ if(isset($_GET['p_id'])) {
         $post_comment_count = $row['post_comment_count'];
         $post_date = $row['post_date'];
     }
+
+    if(isset($_POST['update_post'])) {
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category_id'];
+        $post_author = $_POST['post_author'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        if(empty($post_image)){
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+            $select_image = mysqli_query($connection,$query);
+            while($row=mysqli_fetch_assoc($select_image)){
+                $post_image = $row['post_image'];
+            }
+        }
+
+        $query = "UPDATE posts SET ";
+        $query .= "post_author = '{$post_author}', ";
+        $query .= "post_category_id ='{$post_category_id}', ";
+        $query .= "post_content = '{$post_content}', ";
+        $query .= "post_date = now(), ";
+        $query .= "post_image = '{$post_image}', ";
+        $query .= "post_status = '{$post_status}', ";
+        $query .= "post_tags = '{$post_tags}', ";
+        $query .= "post_title = '{$post_title}' ";
+        $query .= "WHERE post_id = {$the_post_id}";
+        $update_query_result = mysqli_query($connection, $query);
+        confirm($update_query_result);
+        header("Location: posts.php");
+    }
 }
 ?>
 
@@ -24,13 +60,13 @@ if(isset($_GET['p_id'])) {
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title;?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title;?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
         <label for="post_category">Post Category Id</label>
 <!--        <input value="--><?php //echo $post_category_id;?><!--" type="text" class="form-control" name="post_category_id">-->
-        <select name="" id="">
+        <select name="post_category_id" id="post_category_id">
         <?php
         $query = "SELECT * FROM categories";
         $select_all_categories_query=mysqli_query($connection,$query);
@@ -48,7 +84,7 @@ if(isset($_GET['p_id'])) {
 
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input value="<?php echo $post_author;?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author;?>" type="text" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
@@ -58,6 +94,7 @@ if(isset($_GET['p_id'])) {
 
     <div class="form-group">
         <label for="post_image">Post image</label>
+        <input type="file" name="image">
         <img width="100" src="../images/<?php echo $post_image;?>" >
     </div>
 
