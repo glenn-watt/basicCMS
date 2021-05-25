f47468d8a145a01e5bf4127e9237e82f1e62126c
Glenn Watt <glennwatt@me.com>
2021-05-24 23:11:18 -0500

    Add  chart to admin index.php

--- admin/index.php
+++ admin/index.php
@@ -141,6 +141,33 @@
                     </div>
                 </div>
                 <!-- /.row -->
+                <div class="row">
+                    <script type="text/javascript">
+                        google.charts.load('current', {'packages':['bar']});
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable([
+                                ['Data', 'Count'],
+                                ['Posts', 1000]
+
+                            ]);
+
+                            var options = {
+                                chart: {
+                                    title: '',
+                                    subtitle: '',
+                                }
+                            };
+
+                            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                            chart.draw(data, google.charts.Bar.convertOptions(options));
+                        }
+                    </script>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+                </div>
+
             </div>
             <!-- /.container-fluid -->
 
